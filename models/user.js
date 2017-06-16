395d448d39ab9cac4bf3fa3fb2793762d4ae7271
andrewyan-taroko <andrew.yan@tarokosoftware.com>
2017-06-16 17:57:04 +0800

    - sso reference user by sso id insetad of email - add setEmail feature

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const _     = require("lodash");
 const axios = require("axios");
+const GoogleAuth = require("google-auth-library");
 const {
   createJwt,
   verifyJwt,
@@ -7,13 +8,20 @@ const {
   generateID
 } = require("../utils");
 
+const GOOGLE_CLIENT_ID = "251705430658-ohq7k8m1qn0te2rbti8dm7dknjc5da73.apps.googleusercontent.com";
+
+const auth   = new GoogleAuth;
+const client = new auth.OAuth2(GOOGLE_CLIENT_ID, "", "");
+
+const getUserById         = (id) => _.find(users, (user) => user.id === id);
+const getUserByEmail      = (email) => _.find(users, (user) => user.email === email);
+const getUserByAuth       = (email, password) => _.find(users, (user) => user.email === email && user.password === password);
+const getUserByFacebookId = (id) => _.find(users, (user) => user.facebookId === id);
+const getUserByGoogleId   = (id) => _.find(users, (user) => user.googleId === id);
+
 const users = [];
 
-const getUserById    = (id) => _.find(users, (user) => user.id === id);
-const getUserByEmail = (email) => _.find(users, (user) => user.email === email);
-const getUserByAuth  = (email, password) => _.find(users, (user) => user.email === email && user.password === password);
-
-const createUser = ({email, password, confirmPassword, message = "this is a default message", signInWithFacebook = false}) => {
+const createUser = ({ email, password, confirmPassword, message = "this is a default message", facebookId, googleId}) => {
   if(email.length === 0) throw new Error(`email can't be empty`);
   if(getUserByEmail(email)) throw new Error(`email [${email}] has already been taken`);
   if(password !== confirmPassword) throw new Error(`passwords don't match`);
@@ -23,7 +31,8 @@ const createUser = ({email, password, confirmPassword, message = "this is a defa
     email,
     password,
     message,
-    signInWithFacebook
+    facebookId,
+    googleId
   };
   return user;
 };
@@ -36,10 +45,11 @@ const getCurrentUser = ({ req, res }) => {
     const user        = getUserById(id);
     if(user) {
       return {
-        id:                 user.id,
-        email:              user.email,
-        message:            user.message,
-        signInWithFacebook: user.signInWithFacebook
+        id:         user.id,
+        email:      user.email,
+        message:    user.message,
+        facebookId: user.facebookId,
+        googleId:   user.googleId
       };
     }
     throw new Error(`user [${id}] not found`);
@@ -76,15 +86,17 @@ const signInWithFacebook = async ({ userId, accessToken }, { req, res }) => {
         fields:       "id,email"
       }
     });
-    const { email } = data;
-    const user = getUserByEmail(email);
+    const { id: facebookId, email } = data;
+    const userById                  = getUserByFacebookId(facebookId);
+    const userByEmail               = getUserByEmail(email);
+    const user                      = userById || userByEmail;
     if(user) {
       const token = createJwt(user.id);
       res.cookie("token", token);
       return Object.assign({}, user, { token });
     }
     const password = "whatever";
-    const newUser  = createUser({ email, password, confirmPassword: password, signInWithFacebook: true });
+    const newUser  = createUser({ email, password, confirmPassword: password, facebookId });
     users.push(newUser);
     const token = createJwt(newUser.id);
     res.cookie("token", token);
@@ -96,10 +108,40 @@ const signInWithFacebook = async ({ userId, accessToken }, { req, res }) => {
   }
 };
 
+const signInWithGoogle = async ({ token: googleUserToken }, { req, res }) => {
+  try{
+    const data = await new Promise((resolve, reject) => {
+      client.verifyIdToken(googleUserToken, GOOGLE_CLIENT_ID, (err, login) => {
+        if(err) reject(err);
+        else resolve(login.getPayload());
+      });
+    });
+    const { sub: googleId, email } = data;
+    const userById                 = getUserByGoogleId(googleId);
+    const userByEmail              = getUserByEmail(email);
+    const user                     = userById || userByEmail;
+    if(user) {
+      if(!user.googleId) user.googleId = googleId;
+      const token = createJwt(user.id);
+      res.cookie("token", token);
+      return Object.assign({}, user, { token });
+    }
+    const password = "whatever";
+    const newUser  = createUser({ email, password, confirmPassword: password, googleId });
+    users.push(newUser);
+    const token = createJwt(newUser.id);
+    res.cookie("token", token);
+    return Object.assign({}, newUser, { token });
+  }
+  catch(err){
+    throw err;
+  }
+};
+
 const signIn = ({ email, password }, { req, res }) => {
   const user = getUserByAuth(email, password);
   if(user) {
-    if(user.signInWithFacebook) throw new Error("Use your facebook account to sign in!");
+    if(user.createdWithSSO) throw new Error("please sign in with your Facebook or Google account");
     const token = createJwt(user.id);
     res.cookie("token", token);
     return Object.assign({}, user, { token });
@@ -124,6 +166,18 @@ const setMessage = ({ message }, { req, res }) => {
   }
 };
 
+const setEmail = ({ newEmail }, { req, res }) => {
+  try {
+    const { sub: id } = verifyJwt(getToken(req));
+    const user        = getUserById(id);
+    user.email        = newEmail;
+    return user;
+  }
+  catch(err) {
+    throw err;
+  }
+};
+
 // prepare users data
 users.push(createUser({ email: "foobar@test.com", password: "1234", confirmPassword: "1234" }));
 users.push(createUser({ email: "ggyy@test.com", password: "4321", confirmPassword: "4321"}));
@@ -132,8 +186,10 @@ module.exports = {
   getUsers,
   getCurrentUser,
   signInWithFacebook,
+  signInWithGoogle,
   signUp,
   signIn,
   signOut,
-  setMessage
+  setMessage,
+  setEmail
 };
