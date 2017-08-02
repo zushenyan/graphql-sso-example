e1646368522c858b2303b8de55d163b23b23ca4c
Andrew Yan <zushenyan@gmail.com>
2017-08-03 01:31:59 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
+false &&
 echo "Deployment start...."
 
+
+
 # build client
-rm -rf ./public/spa/static
-rm ./public/spa/index.html
-cd ./client
-yarn run build
-cd ..
+rm -rf ./public/spa/static && \
+rm ./public/spa/index.html && \
+cd ./client && \
+yarn install && \
+yarn run build && \
+cd .. && \
 cp -r ./client/dist/* ./public/spa
 
 # databasemirgration
